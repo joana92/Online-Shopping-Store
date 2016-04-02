13820bfc4d576aee99e220494a00faccc90e7a4a
huijie li <joanali1992@gmail.com>
2016-04-02 18:00:23 -0500

    Online shopping store with cart and check out

---
+++ OlineShoppingStore/OnlineShoppingStore.Domain/Concrete/EmailSettings.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineShoppingStore.Domain.Concrete
+{
+   public class EmailSettings
+    {
+        public string MailToAddress = "joanali1992@gmail.com";
+        public string MailFromAddress = "joanali1992@gmail.com";
+        public bool UseSsl = true;
+        public string Username = "joanali1992@gmail.com";
+        public string Password = "huijie123"; // Create your own google app password, In the video I have shown you how to create app password.
+        public string ServerName = "smtp.gmail.com";
+        public int ServerPort = 587;
+    }
+}
---
+++ OlineShoppingStore/WEBUI/Models/CProductListViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using OnlineShoppingStore.Domain.Entities;
+
+namespace WEBUI.Models
+{
+    public class CProductListViewModel
+    {
+        public IEnumerable<Product> Products { get; set; }
+        public PaginInfo pagingInfo { get; set; }
+        public string CurrentCategory { get; set; }
+    }
+}
